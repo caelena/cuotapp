0b61b23ce8ecee395b5b2c3d3a0c2b91eeef1fdf
Luis Ramon Lopez <lrlopez@gmail.com>
2023-12-13 13:44:17 +0100

    New entity: User

---
+++ src/Entity/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'app_user')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 15, unique: true)]
+    private ?string $userName;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(?string $userName): User
+    {
+        $this->userName = $userName;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
